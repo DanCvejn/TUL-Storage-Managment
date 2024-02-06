92874bc77ba435b5a3674b51cde30e5b7dbd918e
DanCvejn <70745296+DanCvejn@users.noreply.github.com>
2024-02-06 21:32:35 +0100

    fix/temp variables remove

--- src/kin/op/cvejn/storagemanager/Testing.java
+++ src/kin/op/cvejn/storagemanager/Testing.java
@@ -24,32 +24,32 @@ public class Testing {
         Product mrkev = new Product("Mrkev", rand.nextInt(46) + 5, rand.nextInt(651) + 50);
         Product okurka = new Product("Okurka", rand.nextInt(46) + 5, rand.nextInt(651) + 50);
         Product rajce = new Product("Rajce", rand.nextInt(46) + 5, rand.nextInt(651) + 50);
-        Product tempProduct = new Product("Meloun", rand.nextInt(46) + 5, rand.nextInt(651) + 50);
-        tempProduct = new Product("Brokolice", rand.nextInt(46) + 5, rand.nextInt(651) + 50);
-        tempProduct = new Product("Celer", rand.nextInt(46) + 5, rand.nextInt(651) + 50);
-        tempProduct = new Product("Redkvicka", rand.nextInt(46) + 5, rand.nextInt(651) + 50);
-        tempProduct = new Product("Svestka", rand.nextInt(46) + 5, rand.nextInt(651) + 50);
-        tempProduct = new Product("Tresen", rand.nextInt(46) + 5, rand.nextInt(651) + 50);
-        tempProduct = new Product("Brambora", rand.nextInt(46) + 5, rand.nextInt(651) + 50);
-        tempProduct = new Product("Cibule", rand.nextInt(46) + 5, rand.nextInt(651) + 50);
-        tempProduct = new Product("Cesnek", rand.nextInt(46) + 5, rand.nextInt(651) + 50);
-        tempProduct = new Product("Hruska", rand.nextInt(46) + 5, rand.nextInt(651) + 50);;
+        Product hruska = new Product("Hruska", rand.nextInt(46) + 5, rand.nextInt(651) + 50);
+        new Product("Meloun", rand.nextInt(46) + 5, rand.nextInt(651) + 50);
+        new Product("Brokolice", rand.nextInt(46) + 5, rand.nextInt(651) + 50);
+        new Product("Celer", rand.nextInt(46) + 5, rand.nextInt(651) + 50);
+        new Product("Redkvicka", rand.nextInt(46) + 5, rand.nextInt(651) + 50);
+        new Product("Svestka", rand.nextInt(46) + 5, rand.nextInt(651) + 50);
+        new Product("Tresen", rand.nextInt(46) + 5, rand.nextInt(651) + 50);
+        new Product("Brambora", rand.nextInt(46) + 5, rand.nextInt(651) + 50);
+        new Product("Cibule", rand.nextInt(46) + 5, rand.nextInt(651) + 50);
+        new Product("Cesnek", rand.nextInt(46) + 5, rand.nextInt(651) + 50);
 
         // Vytvoření skladů
         Storage storage1 = new Storage(10, 1, 1);
         Storage storage2 = new Storage(5, 2, 1);
-        Storage tempStorage = new Storage(10, 1, 2);
-        tempStorage = new Storage(10, 2, 2);
-        tempStorage = new Storage(7, 3, 2);
-        tempStorage = new Storage(5, 3, 1);
+        Storage storage3 = new Storage(5, 3, 1);
+        new Storage(10, 1, 2);
+        new Storage(10, 2, 2);
+        new Storage(7, 3, 2);
 
         // Naplnění skladů produkty
         storage1.storeProduct(jablko);
         storage2.storeProduct(banan);
         storage2.storeProduct(mrkev);
-        tempStorage.storeProduct(okurka);
-        tempStorage.storeProduct(rajce);
-        tempStorage.storeProduct(tempProduct);
-        storage1.storeProduct(tempProduct);
+        storage3.storeProduct(okurka);
+        storage3.storeProduct(rajce);
+        storage3.storeProduct(hruska);
+        storage1.storeProduct(hruska);
     }
 }
